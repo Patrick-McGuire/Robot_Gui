b1f065705c9d1b18acf617a7b0cfd702f5cf555e
nathan <nathan.e.mcguire@gmail.com>
2019-12-27 21:31:23 -0500

    added full screen

--- widgets/VideoScreen.py
+++ widgets/VideoScreen.py
@@ -14,25 +14,33 @@ class VideoScreen(CustomBaseWidget):
     def __init__(self, configDict, window):
         self.window = window
 
-        title = configDict[Constants.TITTLE_ATTRIBUTE] + "\n"
+        title = configDict[Constants.TITTLE_ATTRIBUTE]
         tab = configDict[Constants.TAB_ATTRIBUTE]
         font = configDict[Constants.FONT_ATTRIBUTE]
         fontSize = int(configDict[Constants.FONT_SIZE_ATTRIBUTE])
         xpos = configDict[Constants.X_POS_ATTRIBUTE]
         ypos = configDict[Constants.Y_POS_ATTRIBUTE]
         hidden = configDict[Constants.HIDDEN_ATTRIBUTE]
-        draggabe = configDict[Constants.DRAGGABLE_ATTRIBUTE]
+        draggable = configDict[Constants.DRAGGABLE_ATTRIBUTE]
         foregroundColor = configDict[Constants.FOREGROUND_ATTRIBUTE]
         backgroundColor = configDict[Constants.BACKGROUND_ATTRIBUTE]
         borderwidth = int(configDict[Constants.BORDER_WIDTH_ATTRIBUTE])
         relief = configDict[Constants.RELIEF_ATTRIBUTE]
 
-        videoStream = configDict["config"]
-        self.videoStream = videoStream[0]
+        config = configDict["config"]
+        self.videoStream = config[0]
+        self.width, self.height = config[1].split("x")
+        self.fullScreen = config[2]
+        self.lockAspectRatio = config[3]
+
+        if self.fullScreen:
+            static = True
+        else:
+            static = False
 
         self.configInfo = configDict[Constants.CONFIG_ATTRIBUTE]
 
-        self.widgetTitle = title
+        self.widgetTitle = title + "\n"
 
         self.nameVar = StringVar()
         self.updateInfo(0)
@@ -41,12 +49,15 @@ class VideoScreen(CustomBaseWidget):
         self.widget.grid(column=0, row=0)
         self.widget.place(x=xpos, y=ypos)
 
-        CustomBaseWidget.__init__(self, self.widget, draggabe, xpos, ypos, window,
-                                  configDict[Constants.TITTLE_ATTRIBUTE])
+        CustomBaseWidget.__init__(self, self.widget, draggable, xpos, ypos, window, title, static=static)
 
         self.makeDraggable()
         if hidden:
             self.hide()
+
+    def setDimensions(self, width, height):
+        self.width = width
+        self.height = height
 
     def updateInfo(self, data):
         if data != 0:
@@ -56,11 +67,19 @@ class VideoScreen(CustomBaseWidget):
                 pass
             else:
                 self.hasVideoStream = True
-                self.readStream(frame)
+                self.displayImage(frame)
 
-    def readStream(self, frame):
-        height = 1000
-        frame = cv2.resize(frame, (height, int(height * .75)))
+    def displayImage(self, frame):
+        height, width, channels = frame.shape
+        aspectRatio = float(width)/float(height)
+
+        print(aspectRatio)
+
+        if self.fullScreen:
+            self.width = self.window.winfo_width()
+            self.height = self.window.winfo_height()
+
+        frame = cv2.resize(frame, (int(self.width), int(self.height)))
         frame = cv2.flip(frame, 1)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
         img = Image.fromarray(cv2image)
